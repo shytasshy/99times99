4595d13251881093e70b066f16911f9dd8ae9edb
田島新 <tasshi-@tajimashin-no-MacBook-Pro.local>
2021-07-09 22:45:34 +0900

    カメラ調整開始

--- Assets/Scripts/SceneTransition.cs
+++ Assets/Scripts/SceneTransition.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class SceneTransition : MonoBehaviour
 {
+    [SerializeField] private Camera targetCamera;
+    [SerializeField] private Vector2 aspectVec; 
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +14,28 @@ public class SceneTransition : MonoBehaviour
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        var screenAspect = Screen.width / (float)Screen.height;
+        var targetAspect = aspectVec.x / aspectVec.y;
+
+        var magRate = targetAspect / screenAspect;
+
+        var viewportRect = new Rect(0, 0, 1, 1);
+
+        if (magRate < 1)
+        {
+            viewportRect.width = magRate;
+            viewportRect.x = 0.5f - viewportRect.width * 0.5f;
+        }
+        else
+        {
+            viewportRect.height = 1 / magRate;
+            viewportRect.y = 0.5f - viewportRect.height * 0.5f;
+        }
+
+        targetCamera.rect = viewportRect;
+    }
 
 
     public void LoadRandomCalcScene()
